a56e3c0174f760d9b46fdc4fe8ff73da0cc8bd7d
Hoang Huong | apikey:d0851193d83d8c0dc0833e050f2c432f59dd585145eb42c2440363789246f383 <huong.ph@fsofts.com>
2025-02-26 11:30:27 +0700

    save

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -12,14 +12,23 @@ export default defineConfig({
         outDir: 'public/build',
         emptyOutDir: true,
         manifest: true,
+        chunkSizeWarningLimit: 1000,
         rollupOptions: {
             output: {
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
                         return 'vendor';
                     }
-                }
+                },
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]'
             }
+        }
+    },
+    server: {
+        hmr: {
+            overlay: false
         }
     }
 });
